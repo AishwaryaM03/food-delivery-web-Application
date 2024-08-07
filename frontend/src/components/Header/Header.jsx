3c78360d7b4046baa659a35254e458bda37545f2
Aishwarya Mahajan <130883498+AishwaryaM03@users.noreply.github.com>
2024-08-07 17:57:04 +0530

    commit 2 40.39

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="header-contents">
+            <h2>Order your Favourite food here</h2>
+            <p>Welcome to our convenient online ordering platform! Browse our delicious menu 
+                and order your favorite dishes with just a few clicks. Whether you're craving
+                something savory or sweet, our diverse selection caters to every taste. Enjoy 
+                the ease of choosing your meal online and having it prepared fresh for pickup 
+                or delivery. Start exploring now to treat yourself to a delightful dining 
+                experience, right from the comfort of your home.</p>
+            <button>View Menu</button>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
